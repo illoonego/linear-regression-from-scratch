039c6d928421a1dc3f754f37b8f45def4f8bb727
Illias <illias.dadashov@gmail.com>
2025-11-22 02:47:07 +0000

    Revert "Track models/ source code: remove models/ from .gitignore and add model files"
    This reverts commit cd289c66f044b3767511ccd708d1a1cd21554a72.

--- src/linear_regression/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Models package for linear regression implementations."""
-
-from .linear_regression import LinearRegression
-from .polynomial_regression import PolynomialRegression
-
-__all__ = [
-    "LinearRegression", 
-    "PolynomialRegression"
-]
